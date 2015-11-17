da2250a9c7a54d24d6ede3a1ae5af29b4554ef41
Jorge Fresneda <jorgefresneda@yahoo.es>
2015-11-17 15:49:55 -0500

    Update server.R

--- server.R
+++ server.R
@@ -5,177 +5,28 @@
 # J. Fresneda
 #   11/17/15
 #
-# Using data from Kaggle's
-#   Forest Cover Type Prediction
-#
 
-library(shiny)
-library(randomForest)
-library(caret)
-library(gbm)
-library(plyr)
 
-# We tweak the "am" field to have nicer factor labels. Since this doesn't
-# rely on any user inputs we can do this once at startup and then use the
-# value throughout the lifetime of the application
-Data <- read.csv("./data/Data.csv")
-load("./data/fit.rda")
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+          "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
-#Data$Wilderness_Area1<-factor(Data$Wilderness_Area1)
-
-# Define server logic required to plot various variables against Cover Type
-shinyServer(function(input, output) {
-    
-    # Compute the forumla text in a reactive expression since it is 
-    # shared by the output$caption and output$CoverPlot expressions
-    formulaText <- reactive({
-        paste(input$variable,"~Cover_Type")
-    })
-    
-    # Return the formula text for printing as a caption
-    output$caption <- renderText({
-        formulaText()
-    })
-    
-    # Generate a plot of the requested variable against Cover Type and only 
-    # include outliers if requested
-    output$CoverPlot <- renderPlot({
-        boxplot(as.formula(formulaText()), 
-                data = Data,
-                outline = input$outliers,
-                col="orange",
-                xlab="Cover Type",
-                ylab="Measured")
-    })
-    
-    ## Generate a prediction of which Cover Type you have selected
-    
-    sliderValues <- reactive({
-        
-        data.frame(
-            row.names=c("Elevation",
-                   "Aspect",
-                   "Slope",
-                   "Horizontal_Distance_To_Hydrology",
-                   "Vertical_Distance_To_Hydrology",
-                   "Horizontal_Distance_To_Roadways",
-                   "Hillshade_9am",
-                   "Hillshade_Noon",
-                   "Hillshade_3pm",
-                   "Horizontal_Distance_To_Fire_Points",
-                   "Wilderness_Area1",
-                   "Wilderness_Area2",
-                   "Wilderness_Area3",
-                   "Wilderness_Area4",
-                   "Soil_Type1",
-                   "Soil_Type2",
-                   "Soil_Type3",
-                   "Soil_Type4",
-                   "Soil_Type5",
-                   "Soil_Type6",
-                   "Soil_Type8",
-                   "Soil_Type9",
-                   "Soil_Type10",
-                   "Soil_Type11",
-                   "Soil_Type12",
-                   "Soil_Type13",
-                   "Soil_Type14",
-                   "Soil_Type16",
-                   "Soil_Type17",
-                   "Soil_Type18",
-                   "Soil_Type19",
-                   "Soil_Type20",
-                   "Soil_Type21",
-                   "Soil_Type22",
-                   "Soil_Type23",
-                   "Soil_Type24",
-                   "Soil_Type25",
-                   "Soil_Type26",
-                   "Soil_Type27",
-                   "Soil_Type28",
-                   "Soil_Type29",
-                   "Soil_Type30",
-                   "Soil_Type31",
-                   "Soil_Type32",
-                   "Soil_Type33",
-                   "Soil_Type34",
-                   "Soil_Type35",
-                   "Soil_Type36",
-                   "Soil_Type37",
-                   "Soil_Type38",
-                   "Soil_Type39",
-                   "Soil_Type40"),
-            
-            Value = c(as.integer(input$E),
-                      as.integer(input$A),
-                      as.integer(input$S),
-                      as.integer(input$HW),
-                      as.integer(input$VW),
-                      as.integer(input$HR),
-                      as.integer(input$S9),
-                      as.integer(input$SN),
-                      as.integer(input$S3),
-                      as.integer(input$HF),
-                      as.integer(if(input$Wilderness=="1"){c("1")}else{c("0")}),
-                      as.integer(if(input$Wilderness=="2"){c("1")}else{c("0")}),
-                      as.integer(if(input$Wilderness=="3"){c("1")}else{c("0")}),
-                      as.integer(if(input$Wilderness=="4"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="1"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="2"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="3"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="4"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="5"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="6"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="8"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="9"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="10"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="11"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="12"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="13"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="14"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="16"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="17"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="18"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="19"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="20"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="21"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="22"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="23"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="24"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="25"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="26"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="27"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="28"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="29"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="30"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="31"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="32"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="33"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="34"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="35"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="36"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="37"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="38"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="39"){c("1")}else{c("0")}),
-                      as.integer(if(input$Soil=="40"){c("1")}else{c("0")})
-                      
-                      ),
-        stringsAsFactors=FALSE)        
-    })  
-
-    output$values <- renderTable({
-        sliderValues()
-    })
-    
-    output$ReadMe<-renderPrint({
-        paste("This app was created in R using Shiny.  The data comes from the Kaggle Forest Cover Type Prediction competition found here: http://www.kaggle.com/c/forest-cover-type-prediction/data . There are three tabs on this app.  The Instruction tab which explains how the app works.  The plot tab which allows you to explore the data with different features.  The prediction tab which allows to change different parameters and it will output a prediction of what type of forest cover is at that location.  The prediction model uses a gradient boosted trees method.  This method has a 75.4% accuracy on out of sample data.")
-    })
-    
-    output$Pred<-renderPrint({
-        load("./data/fit.rda")
-        PredVal<-predict(fit,t(sliderValues()))
-        paste("The predicted forest cover type is ",toString(PredVal[1]),sep='')
-    })
-    
-    
+shinyServer(function(input, output, session) {
+  
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+  
 })
