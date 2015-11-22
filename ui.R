3e597e33257c28a924da59011cc77113cafae4a5
Jorge Fresneda <jorgefresneda@yahoo.es>
2015-11-22 13:38:41 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,6 +8,7 @@
 shinyUI(pageWithSidebar(
   headerPanel('k-means clustering of Iris Database'),
   sidebarPanel(
+  helpText('Instructions; This plot shows the within cluster sum of squares as a function of the number of clusters. Choose the type of X variable, the type of Y variable, and the number of cluster.'), 
     selectInput('xcol', 'X Variable', names(iris)),
     selectInput('ycol', 'Y Variable', names(iris),
                 selected=names(iris)[[2]]),
